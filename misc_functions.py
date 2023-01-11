e97707f6928cb4a15d819e197c991c1586b56e19
dan283 <78473045+dan283@users.noreply.github.com>
2023-01-12 11:38:14 +1300

    Create misc_functions.py

---
+++ misc_functions.py
@@ -0,0 +1,16 @@
+import bpy
+
+def add_corrective_smooth(repeat_factor=150):
+    selected_objects = bpy.context.selected_objects
+    for obj in selected_objects:
+        obj.modifiers.new(name="Corrective Smooth", type='CORRECTIVE_SMOOTH')
+        obj.modifiers["Corrective Smooth"].iterations = repeat_factor
+        obj.modifiers["Corrective Smooth"].show_viewport = True
+
+
+def remove_corrective_smooth():
+    selected_objects = bpy.context.selected_objects
+    for obj in selected_objects:
+        for modifier in obj.modifiers:
+            if modifier.type == 'CORRECTIVE_SMOOTH':
+                obj.modifiers.remove(modifier)
